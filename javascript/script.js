08276fe48964b3ae9ff09637f23cc12f2cd5e50f
mas2864 <mas2864@nau.edu>
2026-04-17 16:26:50 -0700

    another attempt

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,21 @@
+let jsonData={};
 function loadJson() {
-  fetch("../json/TeamMembers.json").then(response => response.json()).then(data =>
+  fetch("../JSON/TeamMembers.json").then(response => response.json()).then(data =>
     {
       // store json data in jsonData
-      jsonData = data;
+      jsonData = data.Members;
     }
   )
 }
-
 let namesample="the name";
 let memberList=[];
 
 function sample(){
     memberList=document.getElementById("TeamMembListID");
-    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'> Hola </h4></div>";
+    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'>"+jsonData[1[.Name+" aa</h4></div>";
 }
 
 sample();
+loadJson();
+
+
