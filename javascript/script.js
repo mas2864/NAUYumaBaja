56ab4227d11878e930572e98af2e8f67629ab02e
mas2864 <mas2864@nau.edu>
2026-04-17 21:03:16 -0700

    another one

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,8 @@
-async function getData(){
-    const requestURL="../JSON/members.json";
-    const request=new Request(requestURL);
-    const response=await fetch(request);
-    const jsonData=await response.json();
-    console.log(jsonData);
-    sample(jsonData);
+import * as data from '../JSON/members.json' with { type: 'json' };
+console.log(data);
+function sample(){
+    let memberList= document.getElementById("TeamMembListID");
+    memberList.innerHTML="sddsds";
 }
-getData();
-let memberList=[];
-function sample(data=jsonData){
-    memberList=document.getElementById("TeamMembListID");
-    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'>"+data[1].Name+" aa</h4></div>";
-}
-
 
 
