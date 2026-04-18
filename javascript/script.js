456f4551522d11ba0b80a3b9653cc5dc4d843b3e
mas2864 <mas2864@nau.edu>
2026-04-17 21:16:37 -0700

    capital M in members

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@ import * as data from '../JSON/members.json' with { type: 'json' };
 console.log(data);
 function sample(data){
     let memberList= document.getElementById("TeamMembListID");
-    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'>"+data.members[1].Name+"</h4></div>";
+    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'>"+data.Members[1].Name+"</h4></div>";
 }
 
 sample(data);
