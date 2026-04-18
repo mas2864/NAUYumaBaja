43d30cf77e954305fbba1ac2b638aed12c113a32
mas2864 <mas2864@nau.edu>
2026-04-17 21:12:23 -0700

    script

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,8 @@ import * as data from '../JSON/members.json' with { type: 'json' };
 console.log(data);
 function sample(){
     let memberList= document.getElementById("TeamMembListID");
-    memberList.innerHTML="sddsds";
+    memberList.innerHTML="<div class='MemberCard'><h4 class='CardName'>"+"sample"+"</h4></div>";
 }
 
+sample();
 
